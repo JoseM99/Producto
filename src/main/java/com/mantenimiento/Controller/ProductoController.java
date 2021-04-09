1da9fcb1a778c34afdc560a1af6dc321c86d9893
jose manuel <manuel.f.gutierrez1999@gmail.com>
2021-04-08 22:59:30 -0500

    initial commit

---
+++ src/main/java/com/mantenimiento/Controller/ProductoController.java
@@ -0,0 +1,5 @@
+package com.mantenimiento.Controller;
+
+public class ProductoController {
+
+}
---
+++ src/main/java/com/mantenimiento/ProductosApplication.java
@@ -0,0 +1,13 @@
+package com.mantenimiento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductosApplication.class, args);
+	}
+
+}
